658386eabe13c094f8d2fef3c548578916348033
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2025-05-09 22:23:58 +0200

    Updating the SDL2 Exotique interface to v0.5. Cleaner code. Cleanup function. Proper use on SDL_Log(). New panic function.

--- src/exotique.c
+++ src/exotique.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #pragma GCC diagnostic push
@@ -14,40 +15,11 @@
 
 // XXX: Defines
 
-#ifndef DEBUG
-#define DEBUG 0
-#endif
-
-#ifndef M_PI
-#define M_PI 3.141592653589793238462643383279502884f // pi
-#endif
-
-#ifndef M_PI_2
-#define M_PI_2 1.570796326794896619231321691639751442f // pi/2
-#endif
-
-#ifndef M_3PI_2
-#define M_3PI_2 4.712388980384689857693965074919254326f // (3*pi)/2
-#endif
-
-#ifndef M_2PI
-#define M_2PI 6.283185307179586476925286766559005768f // 2*pi
-#endif
-
-// XXX: This magical number is used to calculate tank sprite rotation
-// It has been discovered intuitively and empiricaly.
-
-#ifndef M_360_2PI
-#define M_360_2PI 57.295779513082320876798154814105170336f // 360/(2*pi)
-#endif
-
-#define kDegreeToRadian (M_PI / 180.0f)
-
-#ifndef kScreenWidth 
+#ifndef kScreenWidth
 #define kScreenWidth 320
 #endif
 #ifndef kScreenHeight
-#define kScreenHeight 240
+#define kScreenHeight 200
 #endif
 #define kScreenPixels (kScreenWidth * kScreenHeight)
 
@@ -76,15 +48,28 @@ enum eKeyState
 };
 typedef enum eKeyState eKeyState;
 
-typedef struct InputManager InputManager;
-struct InputManager
+enum eButton
 {
-  uint16_t buttons[4];
-  int16_t joy_x[4];
-  int16_t joy_y[4];
-  int32_t mouse_x;
-  int32_t mouse_y;
+  bUp,
+  bDown,
+  bLeft,
+  bRight,
+  bSelect,
+  bStart,
+  bA,
+  bB,
+  bX,
+  bY,
+  bL1,
+  bR1,
+  bL2,
+  bR2,
+  bL3,
+  bR3,
+  bMouseLeftClick,
+  bMouseRightClick
 };
+typedef enum eButton eButton;
 
 typedef struct ScreenManager ScreenManager;
 struct ScreenManager
@@ -95,8 +80,6 @@ struct ScreenManager
   bool fullscreen;
   uint8_t* screen;
   uint32_t* screen_rgba;
-  // uint8_t screen[kScreenPixels];
-  // uint32_t screen_rgba[kScreenPixels];
   uint32_t palette[256];
 };
 
@@ -105,11 +88,23 @@ struct GameManager
 {
   const char* name;
   ScreenManager screen_manager;
-  InputManager input_manager;
   bool exit;
-  uint64_t ticks;
   SDL_Scancode key_map[7]; // 7 = eKey_count
   uint8_t key_states[7];   // 7 = eKey_count
+};
+
+typedef struct vec2i_s vec2i_t;
+struct vec2i_s
+{
+  int32_t x;
+  int32_t y;
+};
+
+typedef struct PlayerInput PlayerInput;
+struct PlayerInput
+{
+  uint32_t buttons;
+  vec2i_t joystick;
 };
 
 typedef struct ExotiqueInterface ExotiqueInterface;
@@ -118,24 +113,10 @@ struct ExotiqueInterface
   uint8_t* screen;   //[kScreenWidth * kScreenHeight];
   uint32_t* palette; //[256];
 
-  int32_t* mouse_x;
-  int32_t* mouse_y;
+  vec2i_t mouse;
+  PlayerInput player[4];
 
-  uint16_t* player1_buttons;
-  int16_t* player1_joy_x;
-  int16_t* player1_joy_y;
-
-  uint16_t* player2_buttons;
-  int16_t* player2_joy_x;
-  int16_t* player2_joy_y;
-
-  uint16_t* player3_buttons;
-  int16_t* player3_joy_x;
-  int16_t* player3_joy_y;
-
-  uint16_t* player4_buttons;
-  int16_t* player4_joy_x;
-  int16_t* player4_joy_y;
+  uint64_t ticks;
 };
 
 // XXX: Global data structure
@@ -149,39 +130,20 @@ void game_load(ExotiqueInterface* ei);
 void game_update(ExotiqueInterface* ei);
 void game_draw(ExotiqueInterface* ei);
 
-// XXX: Debug functions
+// XXX: Functions declarations
 
-static void
-debug_printf(const char* fmt, const char* fn, const char* fmt2, ...)
-{
-  va_list ap;
-
-// #pragma clang diagnostic push
-#pragma GCC diagnostic push
-// #pragma clang diagnostic ignored "-Wformat-nonliteral"
-#pragma GCC diagnostic ignored "-Wformat-nonliteral"
-  printf(fmt, fn);
-// #pragma clang diagnostic pop
-#pragma GCC diagnostic pop
-
-  va_start(ap, fmt2);
-  vprintf(fmt2, ap);
-  va_end(ap);
-
-  puts("\033[0m");
-}
-
-inline static void
-sdl_panic_check(const bool condition, const char* function)
-{
-  if (condition)
-  {
-    debug_printf("\033[0;31m[PANIC] SDL_%s(): ", function, "%s", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
-}
+static void exotique_panic(GameManager* gm);
 
 // XXX: Input functions
+
+static inline void
+bit_set(uint32_t* data, uint8_t n)
+{
+  if (n < 32) // Ensure n is within 0-31
+  {
+    *data |= ((uint32_t)1 << n);
+  }
+}
 
 static void
 key_state_update(uint8_t* state, bool is_down)
@@ -200,6 +162,12 @@ key_state_update(uint8_t* state, bool is_down)
   }
 }
 
+static bool
+key_new_get(GameManager* gm, int32_t key)
+{
+  return gm->key_states[key] == eKeyState_pressed;
+}
+
 // XXX: Game functions
 
 static void
@@ -211,16 +179,23 @@ exotique_draw(GameManager* gm)
   {
     sm->screen_rgba[i] = sm->palette[sm->screen[i]];
   }
-  sdl_panic_check(SDL_UpdateTexture(sm->texture, nullptr, sm->screen_rgba, sizeof(uint32_t) * kScreenWidth), "UpdateTexture");
+  if (SDL_UpdateTexture(sm->texture, nullptr, sm->screen_rgba, sizeof(uint32_t) * kScreenWidth))
+  {
+    SDL_LogCritical(SDL_LOG_CATEGORY_RENDER, "Couldn't update the given texture rectangle with new pixel data: %s", SDL_GetError());
+    exotique_panic(gm);
+  }
 
-  // XXX: Rendering the game to a texture
-  // sdl_panic_check(SDL_SetRenderTarget(sm->renderer, sm->texture), "SetRenderTarget");
-  // sdl_panic_check(SDL_RenderClear(sm->renderer), "RenderClear");
-
-  // XXX: Rendering the final texture to screen
-  // sdl_panic_check(SDL_SetRenderTarget(sm->renderer, NULL), "SetRenderTarget");
-  sdl_panic_check(SDL_RenderClear(sm->renderer), "RenderClear");
-  sdl_panic_check(SDL_RenderCopy(sm->renderer, sm->texture, NULL, NULL), "RenderCopy");
+  // Rendering the final texture to screen
+  if (SDL_RenderClear(sm->renderer))
+  {
+    SDL_LogCritical(SDL_LOG_CATEGORY_RENDER, "Couldn't clear the current rendering target with the drawing color: %s", SDL_GetError());
+    exotique_panic(gm);
+  }
+  if (SDL_RenderCopy(sm->renderer, sm->texture, NULL, NULL))
+  {
+    SDL_LogCritical(SDL_LOG_CATEGORY_RENDER, "Couldn't copy a portion of the texture to the current rendering target: %s", SDL_GetError());
+    exotique_panic(gm);
+  }
 
   SDL_RenderPresent(sm->renderer);
 }
@@ -248,18 +223,22 @@ exotique_events(GameManager* gm)
         }
         else if (event.window.event == SDL_WINDOWEVENT_FOCUS_LOST)
         {
-          if (gm->screen_manager.fullscreen)
+          if (!gm->screen_manager.fullscreen)
           {
-            const int sdl_showcursor_result = SDL_ShowCursor(1);
-            sdl_panic_check(sdl_showcursor_result < 0, "ShowCursor");
+            if (SDL_ENABLE == SDL_ShowCursor(SDL_ENABLE))
+            {
+              SDL_LogWarn(SDL_LOG_CATEGORY_INPUT, "Couldn't toggle whether or not the cursor is shown: %s", SDL_GetError());
+            }
           }
         }
         else if (event.window.event == SDL_WINDOWEVENT_FOCUS_GAINED)
         {
-          if (gm->screen_manager.fullscreen)
+          if (!gm->screen_manager.fullscreen)
           {
-            const int sdl_showcursor_result = SDL_ShowCursor(0);
-            sdl_panic_check(sdl_showcursor_result < 0, "ShowCursor");
+            if (SDL_DISABLE == SDL_ShowCursor(SDL_DISABLE))
+            {
+              SDL_LogWarn(SDL_LOG_CATEGORY_INPUT, "Couldn't toggle whether or not the cursor is shown: %s", SDL_GetError());
+            }
           }
         }
         break;
@@ -271,11 +250,15 @@ exotique_events(GameManager* gm)
 
   int numkeys = 0;
   const uint8_t* keystate = SDL_GetKeyboardState(&numkeys);
-  sdl_panic_check(!keystate, "GetKeyboardState");
+  if (!keystate)
+  {
+    SDL_LogCritical(SDL_LOG_CATEGORY_INPUT, "Couldn't get a snapshot of the current state of the keyboard: %s", SDL_GetError());
+    exotique_panic(gm);
+  }
 
   for (size_t i = 0; i < eKey_count; ++i)
   {
-    const int scancode = gm->key_map[i];
+    const int scancode = (int)gm->key_map[i];
 
     bool is_down = false;
 
@@ -293,34 +276,22 @@ exotique_load(GameManager* gm, ExotiqueInterface* ei)
 {
   ScreenManager* sm = &gm->screen_manager;
 
-  gm->name = "🌴 Exotique - Preview Developer Version 0.1 - 24/09/18";
+  gm->name = "🌴 Exotique v0.5β - SDL2 (25/05/09)";
 
   sm->screen = malloc(kScreenWidth * kScreenHeight * sizeof(uint8_t));
   sm->screen_rgba = malloc(kScreenWidth * kScreenHeight * sizeof(uint32_t));
 
+  gm->key_map[eKey_up] = SDL_SCANCODE_UP;
+  gm->key_map[eKey_down] = SDL_SCANCODE_DOWN;
+  gm->key_map[eKey_left] = SDL_SCANCODE_LEFT;
+  gm->key_map[eKey_right] = SDL_SCANCODE_RIGHT;
+  gm->key_map[eKey_shoot] = SDL_SCANCODE_RETURN;
+
   ei->screen = gm->screen_manager.screen;
   ei->palette = gm->screen_manager.palette;
 
-  ei->mouse_x = &gm->input_manager.mouse_x;
-  ei->mouse_y = &gm->input_manager.mouse_y;
-
-  ei->player1_buttons = &gm->input_manager.buttons[0];
-  ei->player1_joy_x = &gm->input_manager.joy_x[0];
-  ei->player1_joy_y = &gm->input_manager.joy_y[0];
-
-  ei->player2_buttons = &gm->input_manager.buttons[1];
-  ei->player2_joy_x = &gm->input_manager.joy_x[1];
-  ei->player2_joy_y = &gm->input_manager.joy_y[1];
-
-  ei->player3_buttons = &gm->input_manager.buttons[2];
-  ei->player3_joy_x = &gm->input_manager.joy_x[2];
-  ei->player3_joy_y = &gm->input_manager.joy_y[2];
-
-  ei->player4_buttons = &gm->input_manager.buttons[3];
-  ei->player4_joy_x = &gm->input_manager.joy_x[3];
-  ei->player4_joy_y = &gm->input_manager.joy_y[3];
-
-  //memset(&ei->input, 0, sizeof(ei->input));
+  memset(&ei->mouse, 0, sizeof(ei->mouse));
+  memset(&ei->player, 0, sizeof(ei->player));
 }
 
 static void
@@ -333,14 +304,32 @@ exotique_unload(GameManager* gm)
 }
 
 static void
-exotique_update(GameManager* gm)
+exotique_update(GameManager* gm, ExotiqueInterface* ei)
 {
-  gm->ticks = SDL_GetTicks64();
+  ei->ticks = SDL_GetTicks64();
 
-  SDL_GetMouseState(&gm->input_manager.mouse_x, &gm->input_manager.mouse_y);
+  memset(&ei->mouse, 0, sizeof(ei->mouse));
+  SDL_GetMouseState(&ei->mouse.x, &ei->mouse.y);
   //SDL_GetGlobalMouseState(&gm->input_manager.mouse_x, &gm->input_manager.mouse_y);
 
+  memset(&ei->player, 0, sizeof(ei->player));
   exotique_events(gm);
+  if (key_new_get(gm, eKey_up))
+  {
+    bit_set(&ei->player[0].buttons, bUp);
+  }
+  if (key_new_get(gm, eKey_down))
+  {
+    bit_set(&ei->player[0].buttons, bDown);
+  }
+  if (key_new_get(gm, eKey_left))
+  {
+    bit_set(&ei->player[0].buttons, bLeft);
+  }
+  if (key_new_get(gm, eKey_right))
+  {
+    bit_set(&ei->player[0].buttons, bRight);
+  }
 }
 
 // XXX: SDL functions
@@ -350,22 +339,42 @@ sdl_load(GameManager* gm)
 {
   ScreenManager* sm = &gm->screen_manager;
 
-  puts("[INFO]: SDL2 initialization");
-  sdl_panic_check(SDL_Init(SDL_INIT_VIDEO), "Init");
+  SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+  SDL_LogInfo(SDL_LOG_CATEGORY_SYSTEM, "SDL2 initialization");
+  if (SDL_Init(SDL_INIT_VIDEO))
+  {
+    SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Couldn't initialize the SDL library: %s", SDL_GetError());
+    exotique_panic(gm);
+  }
 
-  sm->window = SDL_CreateWindow(gm->name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, kScreenWidth, kScreenHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_UTILITY);
-  sdl_panic_check(!sm->window, "CreateWindow");
+  if (!(sm->window = SDL_CreateWindow(gm->name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, kScreenWidth, kScreenHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_UTILITY)))
+  {
+    SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Couldn't create a window with the specified position, dimensions, and flags: %s", SDL_GetError());
+    exotique_panic(gm);
+  }
 
-  sm->renderer = SDL_CreateRenderer(sm->window, -1, /*0*/SDL_RENDERER_PRESENTVSYNC);
-  sdl_panic_check(!sm->renderer, "CreateRenderer");
+  if (!(sm->renderer = SDL_CreateRenderer(sm->window, -1, SDL_RENDERER_PRESENTVSYNC)))
+  {
+    SDL_LogCritical(SDL_LOG_CATEGORY_RENDER, "Couldn't create a 2D rendering context for a window: %s", SDL_GetError());
+    exotique_panic(gm);
+  }
 
-  sm->texture = SDL_CreateTexture(sm->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, kScreenWidth, kScreenHeight);
-  sdl_panic_check(!sm->texture, "CreateTexture");
+  if (!(sm->texture = SDL_CreateTexture(sm->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, kScreenWidth, kScreenHeight)))
+  {
+    SDL_LogCritical(SDL_LOG_CATEGORY_RENDER, "Couldn't create a texture for a rendering context: %s", SDL_GetError());
+    exotique_panic(gm);
+  }
 
   // These calls are optional.
   // They only serves to make the pixels square and clean when resizing the window.
-  sdl_panic_check(SDL_RenderSetLogicalSize(sm->renderer, kScreenWidth, kScreenHeight), "RenderSetLogicalSize");
-  sdl_panic_check(SDL_RenderSetIntegerScale(sm->renderer, SDL_TRUE), "RenderSetIntegerScale");
+  if (SDL_RenderSetLogicalSize(sm->renderer, kScreenWidth, kScreenHeight))
+  {
+    SDL_LogWarn(SDL_LOG_CATEGORY_RENDER, "Couldn't create a texture for a rendering context: %s", SDL_GetError());
+  }
+  if (SDL_RenderSetIntegerScale(sm->renderer, SDL_TRUE))
+  {
+    SDL_LogWarn(SDL_LOG_CATEGORY_RENDER, "Couldn't set whether to force integer scales for resolution-independent rendering: %s", SDL_GetError());
+  }
 }
 
 static void
@@ -376,6 +385,21 @@ sdl_unload(GameManager* gm)
   SDL_DestroyTexture(sm->texture);
   SDL_DestroyRenderer(sm->renderer);
   SDL_DestroyWindow(sm->window);
+}
+
+static void
+exotique_cleanup(GameManager* gm)
+{
+  sdl_unload(gm);
+  exotique_unload(gm);
+  SDL_Quit();
+}
+
+static void
+exotique_panic(GameManager* gm)
+{
+  exotique_cleanup(gm);
+  exit(EXIT_FAILURE);
 }
 
 // XXX: Main function
@@ -390,18 +414,15 @@ main(const int argc, const char* argv[])
   game_load(&g_exotique_interface);
   sdl_load(&g_game_manager);
 
-  // XXX: Main loop
+  // Main loop
   while (!g_game_manager.exit)
   {
-    exotique_update(&g_game_manager);
+    exotique_update(&g_game_manager, &g_exotique_interface);
     game_update(&g_exotique_interface);
     game_draw(&g_exotique_interface);
     exotique_draw(&g_game_manager);
   }
 
-  sdl_unload(&g_game_manager);
-  exotique_unload(&g_game_manager);
-  SDL_Quit();
-
+  exotique_cleanup(&g_game_manager);
   return EXIT_SUCCESS;
 }
