66132ae5cce67328c1e016ca67941ebad1e34515
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2026-05-08 17:04:10 +0200

    clarifying the `build.sh` script
    Signed-off-by: Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>

--- src/examples/build.sh
+++ src/examples/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+DEBUG="-O0 -ggdb3 -fsanitize=address"
 EXTRA_CFLAGS="-Waggregate-return -Warith-conversion -Wcast-align=strict -Wcast-qual -Wconversion -Wdouble-promotion -Wduplicated-branches -Wduplicated-cond -Wfloat-equal -Wformat=2 -Wlogical-op -Wmissing-include-dirs -Wnull-dereference -Wstrict-aliasing=2 -Wstrict-overflow=2 -Wswitch-default -Wswitch-enum -Wundef -Wuninitialized -Wwrite-strings"
-CFLAGS="-Wall -Wextra -Wpedantic -Wshadow -Werror -Wfatal-errors $EXTRA_CFLAGS"
-ARCH=64
+CFLAGS="-Wall -Wextra -Wpedantic -Wshadow -Werror -Wfatal-errors $EXTRA_CFLAGS $DEBUG -I ../"
+
 if [ -z "$1" ]; then
   echo "Usage: $0 <game_name>"
   exit 1
@@ -10,6 +11,7 @@ fi
 
 GAME="${1%.c}"
 
-gcc -c $CFLAGS -fsanitize=address -std=c23 -O0 -ggdb3 -I /usr/include/SDL2 -D_REENTRANT ../exotique.c
-gcc -c $CFLAGS -fsanitize=address -std=c89 -O0 -ggdb3 -I ../ -nostdlib -nostdinc -nodefaultlibs -nolibc -ffreestanding -D ARCH=$ARCH "$GAME".c
-gcc exotique.o "$GAME".o -lasan -lSDL2 -o "$GAME" -fsanitize=address
+gcc -c $CFLAGS -std=c23 -I /usr/include/SDL2 -D_REENTRANT ../exotique.c
+gcc -c $CFLAGS -std=c89 -nostdlib -nostdinc -nodefaultlibs -nolibc -ffreestanding "$GAME".c
+
+gcc exotique.o "$GAME".o -o "$GAME" -lSDL2 -lasan -fsanitize=address
